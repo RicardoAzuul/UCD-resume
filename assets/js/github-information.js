4f9d02f50b24d978b851f5043ccdfef42e7782ab
Richard Blaauw <richard.blaauw@ogd.nl>
2021-05-24 16:15:08 +0200

    Add code that fetches repo information and displays that.

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -18,6 +18,31 @@ function userInformationHTML(user) {
   `
 }
 
+function repoInformationHTML(repos) {
+  if (repos.length === 0 ) {
+    return `<div class="clearfix repo-list">No public repos!</div>`;
+  }
+  
+  let listItemsHTML = repos.map(function(repo) {
+    return `
+      <li>
+        <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+      </li>
+    `
+  });
+
+  return `
+  <div class="clearfix repo-list">
+    <p>
+      <strong>Repo List:</strong>
+    </p>
+    <u>
+      ${listItemsHTML.join("\n")}
+    </ul>
+  </div>
+  `;
+}
+
 function fetchGitHubInformation(event) {
 
   var username = $('#gh-username').val();
@@ -32,12 +57,15 @@ function fetchGitHubInformation(event) {
     </div>`);
 
   $.when(
-    $.getJSON(`https://api.github.com/users/${username}`)
+    $.getJSON(`https://api.github.com/users/${username}`),
+    $.getJSON(`https://api.github.com/users/${username}/repos`)
   )
     .then(
-      function (response) {
-        let userData = response;
+      function (firstResponse, secondResponse) {
+        let userData = firstResponse[0];
+        let repoData = secondResponse[0];
         $('#gh-user-data').html(userInformationHTML(userData));
+        $('#gh-repo-data').html(repoInformationHTML(repoData));
       }, function (errorResponse) {
         if (errorResponse.status === 404) {
           $('#gh-user-data').html(
