01a830428fd7be6936ac9179aa1762a8e072a70b
Richard Blaauw <richard.blaauw@ogd.nl>
2021-05-25 19:43:50 +0200

    Get rid of lingering user and repo data.

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -44,6 +44,9 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
+  $('#gh-user-data').html('');
+  $('#gh-repo-data').html('');
+
 
   var username = $('#gh-username').val();
   if (!username) {
